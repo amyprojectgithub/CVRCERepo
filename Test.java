3fab0d0d5840a5532a20a6dea889036770cbcce9
DEV <DEV@gmail.com>
2020-05-24 07:03:29 +0530

    CR-34

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Demo{
    public static void main(String[] args){
-
+       int a=65;
 }
 
 }
--- User.java
+++ User.java
@@ -1,8 +1,11 @@
 class Demo{
    public static void main(String[] args){
-  int j=30;
-  int k=22;
-System.out.println("Hello");
+   m1();
 }
+
+ public static void m1(){
+
+}
+
 
 }
